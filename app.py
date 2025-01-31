0a64d38cc5a809f875d9a028f92ddb58af8be1e1
VijayWhat <vijayrv042@gmail.com>
2025-01-31 16:03:05 +0530

    Adding about page

--- app.py
+++ app.py
@@ -9,5 +9,9 @@ def home():
     name = "VJ"
     return render_template('home.html' , name = name)
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
